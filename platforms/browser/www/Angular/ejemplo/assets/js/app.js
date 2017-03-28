c2fb7dc30bc3e12c731aaa64d0b811535103ecb5
javical <javicentecalderon@gmail.com>
2017-03-28 20:16:02 +0200

    cambios
    app

--- platforms/browser/www/Angular/ejemplo/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    var input = document.getElementById('name'),
-        span = document.getElementById('target');
-
-    input.addEventListener ('keyop', function(e){
-            console.log ('change event');
-            span.innerText = ths.value;
-    }, false );
-};
--- platforms/browser/www/JavaScript/ej07/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    
-    var validarEmail = function (email){
-        return (/^\w([\w.\-]*\w)?@[a-zA-Z0-9]([\w.\-]*\w)?\.[a-zA-Z]{2,3}$/).test(email);
-    };
-
-})();
